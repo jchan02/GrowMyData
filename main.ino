b76e4bc9e3f9119917a48704e9b14b67f64669d4
jchan02 <58665835+jchan02@users.noreply.github.com>
2020-11-29 23:30:11 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,15 +1,21 @@
 #include "Adafruit_seesaw.h"
 #include "Adafruit_ADS1015.h"
-#include <Wire.h>
+#include "DHT.h"
+#include "Wire.h"
+
+#define DHTTYPE DHT11
 
 Adafruit_seesaw stemma1;
 Adafruit_seesaw stemma2;
 Adafruit_ADS1115 adc;
+DHT dht(1, DHTTYPE);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   stemma1.begin(0x36);
   stemma2.begin(0x37);
+  adc.begin();
+  dht.begin();
 }
 
 void loop() {
@@ -20,7 +26,7 @@ void loop() {
   float temp2 = stemma2.getTemp();
   uint16_t moist2 = stemma2.touchRead(0);
   light = adc.readADC_SingleEnded(0);
-  humidity = adc.readADC_SingleEnded(1);
+  float humidity = dht.readHumidity();
 
   Serial.print("Temperature (1): "); Serial.print(temp1); Serial.println("C");
   Serial.print("Capacitive (1): "); Serial.println(moist1);
@@ -28,5 +34,5 @@ void loop() {
   Serial.print("Capacitive (2): "); Serial.println(moist2);
   Serial.print("Light: "); Serial.println(light);
   Serial.print("Humidity: "); Serial.println(humidity);
-  delay(100);
+  delay(1000);
 }
